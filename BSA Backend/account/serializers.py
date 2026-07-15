00ec5b8d2a368bb579fe53240d4c7b93ac51335b
Yuvraj Soni <yuvrajsoni9192@gmail.com>
2026-07-15 17:31:45 +0530

    15-07-2026

--- BSA Backend/account/serializers.py
+++ BSA Backend/account/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import AccountDetail
 
+from django.contrib.auth import authenticate
+from rest_framework.serializers import ModelSerializer
+from django.db import models
 
-
-
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 # class AccountDetailSerializer(serializers.ModelSerializer):
@@ -35,7 +37,66 @@ class RegisterSerializer(serializers.ModelSerializer):
         user = AccountDetail.objects.create_user(**validated_data)
         user.set_password(validated_data['password'])
         user.save()
-        
         return user
     
+class RegisterSerializerShop(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(write_only=True, required=True)
+    class Meta:
+        model = AccountDetail
+        fields = ['first_name','middle_name' , 'last_name', 'username','email' ,'phone','shop_name' ,'shop_address','profile_picture','password','confirm_password']
     
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        user = AccountDetail.objects.create_user(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    
+    def validate(self, data):
+        user = authenticate(
+            username=data.get("username"),
+            password=data.get("password")
+        )
+
+        if user is None:
+            raise serializers.ValidationError({
+                "detail": "Invalid username or password."
+            })
+
+        data["user"] = user
+        return data
+
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+    
+    def validate(self, attrs):
+        self.token = attrs["refresh"]
+        return attrs
+    def save(self):
+        try:
+            RefreshToken(self.token).blacklist()
+        except Exception as e:
+            print("Logout Error:", e)
+            raise serializers.ValidationError({
+                "refresh": str(e)
+            })
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccountDetail
+        fields = [
+            'id','username','first_name','last_name','email','phone_number','role','profile_picture'
+            
+        ]
+
